dc914993d2b34e880c48506bf5930791f58a6f42
Satyajit Paul <paulsjit@gmail.com>
2023-06-11 02:30:50 +0530

    Test Com

--- api/index.js
+++ api/index.js
@@ -1,25 +1,43 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
 
-const httpServer = createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
-});
-const io = new Server(httpServer, { 
-    cors: {
-        origin: "*",
-      }
- });
+// const httpServer = createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end("Hello World!");
+// });
+// const io = new Server(httpServer, { 
+//     cors: {
+//         origin: "*",
+//       }
+//  });
 
-var numberOfClients = 0;
-io.on("connection", (socket) => {
-    numberOfClients++;
-    // console.log("a user connected"+socket.id);
-    io.sockets.emit("numberOfClients", {numberOfClients: numberOfClients});
-    socket.on("disconnect", () => {
-        numberOfClients--;
-        // console.log("user disconnected"+socket.id);
-        io.sockets.emit("numberOfClients", {numberOfClients: numberOfClients});
-    })
+// var numberOfClients = 0;
+// io.on("connection", (socket) => {
+//     numberOfClients++;
+//     // console.log("a user connected"+socket.id);
+//     io.sockets.emit("numberOfClients", {numberOfClients: numberOfClients});
+//     socket.on("disconnect", () => {
+//         numberOfClients--;
+//         // console.log("user disconnected"+socket.id);
+//         io.sockets.emit("numberOfClients", {numberOfClients: numberOfClients});
+//     })
+// });
+// httpServer.listen(3000);
+
+import express from 'express'
+const app = express();
+
+
+app.get('/api', (req, res) => {
+  const path = `/api/item`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
-httpServer.listen(3000);
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+export default app
